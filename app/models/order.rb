0594c084c67a9da5fbbfdefdb86546e20688b956
okamonta <kb24ko52@gmail.com>
2021-10-18 14:11:48 +0000

    environment finish

---
+++ app/models/order.rb
@@ -0,0 +1,7 @@
+class Order < ApplicationRecord
+  enum payment_method: { credit_card: 0, transfer: 1 }
+  enum status: { wait: 0, confirm: 1, making: 2, prepare: 3, finish: 4 }
+  
+  belongs_to :customer
+  has_many :order_details, dependent: :des
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,39 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  
+  namespace :public do
+    root to: 'homes#top'
+    get 'homes/about' => 'homes#about'
+    resources :customers, only: [:show, :edit, :update] do
+      get 'confirm'
+      patch 'leave'
+    end
+    resources :products, only: [:index, :show]
+    resources :cart_items, only: [:index, :create, :update, :destroy] do
+      delete 'cart_items' => 'cart_items#destroy_all', as: "destroy"
+    end
+    resources :orders, only: [:new, :create, :index, :show] do
+      get 'confirm'
+      get 'complete'
+    end
+    resources :deliveries, only: [:index, :create, :destroy, :edit, :update]
+  end
+  
+  namespace :admin do
+    root to: 'homes#top'
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :products, only: [:index, :new, :create, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :order_details, only: [:update]
+    resources :genres, only: [:index, :create, :edit, :update]
+  end
+  
+  devise_for :customers, controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+  devise_for :admin, controllers: {
+  sessions: "admin/sessions"
+}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
