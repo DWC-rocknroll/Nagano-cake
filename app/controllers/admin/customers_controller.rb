e423ed98c8014236b01b1332880874e48d7b157b
“K-takashii” <nanase.sigre.1627.yorushika@gmail.com>
2021-10-21 04:13:48 +0000

    Fix

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,8 +1,12 @@
 class Admin::CustomersController < ApplicationController
+  before_action :authenticate_admin!
+  
   def index
+    @customer = Customer.new
   end
 
   def show
+    
   end
 
   def edit
