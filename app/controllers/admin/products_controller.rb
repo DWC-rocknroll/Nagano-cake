1ca0d184cd7a66c6c644d60ca6de038b9e645942
ss473901 <satoshi.473901.1205@gmail.com>
2021-10-21 13:34:28 +0000

    add

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -3,17 +3,33 @@ class Admin::ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
   end
 
   def create
+    product = Product.new(product_params)
+    product.save
+    redirect_to admin_product_path(product.id)
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(
+    :genre_id,
+    :name,
+    :introduction,
+    :tax_out_price,
+    :image,
+    :is_sale_active,)
   end
 end
