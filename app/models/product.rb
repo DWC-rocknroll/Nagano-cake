81ec129c4bfec0cd2f7a2b0395d4e38f96262f4c
okamonta <kb24ko52@gmail.com>
2021-10-23 08:13:02 +0000

    fixed products_controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,8 @@ class Product < ApplicationRecord
   has_many :cart_items
   has_many :order_details
   belongs_to :genre
+  attachment :image
+  def add_tax_tax_out_price
+    (self.tax_out_price * 1.10).round
+  end
 end
