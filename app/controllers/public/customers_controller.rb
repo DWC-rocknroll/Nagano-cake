102d7c0d796b146fcb1bc3c875de64cd17f5ce0f
syunki4224 <syunkikoike4224@gmail.com>
2021-10-23 12:53:39 +0000

    P/customer/edit.leave_add by syunki

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,14 +4,30 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = current_customer
   end
 
   def update
+    customer = current_customer
+    customer.update(customer_params)
+    redirect_to public_customer_path(current_customer)
   end
 
   def confirm
   end
 
   def leave
+    customer = current_customer
+    customer.is_active = false
+    customer.save
+    reset_session
+    redirect_to public_root_path
   end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :residence, :phone_number, :email)
+  end
+
 end
