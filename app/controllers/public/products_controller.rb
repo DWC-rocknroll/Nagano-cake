531be11594265c0c26671dd746cadf9e18a02480
okamonta <kb24ko52@gmail.com>
2021-10-23 13:05:38 +0000

    Add public/products

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,10 +1,12 @@
 class Public::ProductsController < ApplicationController
   
   def index
-    @products = Product.all
+    @products = Product.where(is_sale_active: true).page(params[:page]).per(8)
+    @quantity = Product.count
   end
 
   def show
     @product = Product.find(params[:id])
+    @cart_item = CartItem.new
   end
 end
