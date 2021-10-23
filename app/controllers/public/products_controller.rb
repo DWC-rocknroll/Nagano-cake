1f12d16e3eed354b1eb3f65c77f75a5ca307be98
ss473901 <satoshi.473901.1205@gmail.com>
2021-10-23 11:15:20 +0000

    Add cart_items

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,7 +1,11 @@
 class Public::ProductsController < ApplicationController
+
   def index
+    @produ
   end
 
   def show
+    @product = Product.find(params[:id])
+    @cart_item = CartItem.new
   end
 end
