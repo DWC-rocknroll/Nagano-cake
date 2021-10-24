e993ee4daeef559933f885d7685bbe1bf51df75b
ss473901 <satoshi.473901.1205@gmail.com>
2021-10-24 07:12:56 +0000

    Add sub_price

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,7 +1,7 @@
 class Public::CartItemsController < ApplicationController
   def index
     @cart_item = CartItem.all
-
+    @cart_item_quantity = CartItem.new
   end
 
   def create
@@ -12,6 +12,9 @@ class Public::CartItemsController < ApplicationController
   end
 
   def update
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to public_cart_items_path
   end
 
   def destroy
