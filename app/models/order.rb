da5f9d956be5ea3fa575a164a3cfdb8b165cf161
okamonta <kb24ko52@gmail.com>
2021-10-27 05:25:11 +0000

    Add validates

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,11 +6,4 @@ class Order < ApplicationRecord
   has_many :order_details, dependent: :destroy
   has_many :products, through: :order_details
 
-  validates :postage, presence: true
-  validates :total_price, presence: true
-  validates :payment_method, presence: true
-  validates :name, presence: true
-  validates :deliveries_postcode, presence: true
-  validates :address, presence: true
-  validates :status, presence: true
 end
